057eb39648000850772318e6a60b601fd36e12a5
MelindaSW <sandstrommelinda@gmail.com>
2023-10-23 14:19:00 +0200

    add modal component

--- src/components/MovieCard/MovieCards.tsx
+++ src/components/MovieCard/MovieCards.tsx
@@ -1,30 +1,49 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import './Moviecards.css';
 
 import React, { FormEvent, useState } from 'react';
 
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import Modal from '../Modal/Modal';
 
 // interface IMovieCardProps {}
 
 const MovieCards = () => {
   const dispatch = useAppDispatch();
   const movies = useAppSelector((state) => state.movies.movies);
+  const [showMovieDetails, setShowMovieDetails] = useState(false);
+
+  const handleOnClick = () => {
+    setShowMovieDetails(true);
+    console.log(showMovieDetails);
+  };
 
   return (
-    <ul>
-      {movies[0].Title != '' ? (
-        movies.map((m, index) => (
-          <li key={index}>
-            <img src={m.Poster} alt="poster" />
-            <div className="Movie-card-title">
-              <span>{m.Title}</span>
-            </div>
-          </li>
-        ))
-      ) : (
-        <></>
-      )}
-    </ul>
+    <>
+      <ul>
+        {movies[0].Title != '' ? (
+          movies.map((m, index) => (
+            <li key={index}>
+              <button className="imagebutton" onClick={handleOnClick}>
+                <img src={m.Poster} alt="poster" />
+              </button>
+            </li>
+          ))
+        ) : (
+          <></>
+        )}
+      </ul>
+      <Modal
+        isOpen={showMovieDetails}
+        hasCloseBtn={true}
+        onClose={() => {
+          console.log('closing modal');
+          setShowMovieDetails(false);
+        }}
+      >
+        <div>testing content</div>
+      </Modal>
+    </>
   );
 };
 
